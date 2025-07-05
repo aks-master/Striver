18dcb7a50b48fd536eeed8b7103a2d1b8c8c02bf
Abhishek Kumar Singh <abhishekse@outlook.com>
2025-07-05 22:19:06 +0530

    pattern questions 1-5

---
+++ 1.Patterns/pattern1.cpp
@@ -0,0 +1,30 @@
+/*
+
+Input: N = 6
+Output:
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
